07fdcc483f4dd9ccfc5d5b79f6b313a073e91a7f
Dongjun.Wei <weidongjun123@gmail.com>
2019-08-10 19:26:26 +0800

    update output

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 rm -r summ
 mkdir summ
-cp -r ../model/dbpedia/ summ
-cp -r ../model/lmdb/ summ
-java -jar esummeval_v1.1.jar ESBM_benchmark_v1.1/ summ/
+cp -r ../model/dbpedia/ output
+cp -r ../model/lmdb/ output
+java -jar esummeval_v1.1.jar ESBM_benchmark_v1.1/ output/
 
